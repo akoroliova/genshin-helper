3571f842eff6a7c9858f804eb8eab232004f6c91
akoroliova <a.koroliova@gmail.com>
2024-05-23 16:54:23 +0300

    Complete main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import App from "./components/App.jsx";
+import App from "./components/App/App.jsx";
 import { store, persistor } from "./redux/store";
 import "modern-normalize";
-import "./main.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
