c1ab73b7a57c23f7f438df129c1ba52000aac4a3
akoroliova <a.koroliova@gmail.com>
2024-05-23 16:43:11 +0300

    Add primitive layout for ToDoToday

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import App from "./components/App.jsx";
+import { store, persistor } from "./redux/store";
+import "modern-normalize";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/pages/ToDoTodayPage/ToDoToday.jsx
@@ -0,0 +1,20 @@
+export default function ToDoToday() {
+  return (
+    <hr>
+      <h1>ToDo Today</h1>
+      <p>...in descending priority order...</p>
+      <hr />
+      <h2>Dailies</h2>
+      <div></div>
+      <hr />
+      <h2>Farm Today (aggregated from FarmDashboard)</h2>
+      <div></div>
+      <hr />
+      <h2>Weeklies (until next Monday)</h2>
+      <div></div>
+      <hr />
+      <h2>Manual ToDos (can be with or without indiidual timers)</h2>
+      <div></div>
+    </hr>
+  );
+}
