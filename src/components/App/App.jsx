70a75001478a491f6e5282890f9a0ff9e718e813
akoroliova <a.koroliova@gmail.com>
2024-05-27 13:33:35 +0300

    Add FormikForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./App.css";
 import initialTasks from "../../assets/initialTasks.json";
 import Filter from "../../pages/ToDoTodayPage/Filter/Filter";
 import TaskList from "../../pages/ToDoTodayPage/TaskList/TaskList";
@@ -6,7 +7,7 @@ import UncontrolledInput from "../utilityParts/forms/UncontrolledInput";
 import ControlledInput from "../utilityParts/forms/ControlledInput";
 import SelectDropdown from "../utilityParts/forms/SelectDropdown";
 import MultipleDropdown from "../utilityParts/forms/MultipleDropdown";
-import "./App.css";
+import FormikForm from "../utilityParts/forms/FormikForm";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
@@ -33,8 +34,16 @@ function App() {
     console.log("User has selected: ", newChoice);
   };
 
+  const handleAddUser = (newUser) => {
+    console.log("handleAddUser", newUser);
+  };
+
   return (
     <>
+      <div>
+        <h1>Forms with Formik</h1>
+        <FormikForm onAdd={handleAddUser} />
+      </div>
       <UncontrolledInput onAdd={addTask} />
       <Filter value={filter} onFilter={setFilter} />
       <TaskList items={visibleTasks} onDelete={deleteTask} />
