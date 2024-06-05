49b9eba510f69bf6c9f91c008480568283652e51
Oleksandra K <a.koroliova@gmail.com>
2024-06-05 15:07:01 +0300

    Add icon fetch for character card

--- src/pages/CharBuilderPage/CharPicker/CharacterCard/CharacterCard.jsx
+++ src/pages/CharBuilderPage/CharPicker/CharacterCard/CharacterCard.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import { getCharacterIcon } from "../../../characters-api";
+
 export default function CharacterCard({
   character: { id, name, rarity, vision_key, weapon_type },
 }) {
+  const [characterIcon, setCharacterIcon] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchCharacterIcon() {
+      try {
+        setLoading(true);
+        const data = await getCharacterIcon(id);
+        setCharacterIcon(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCharacterIcon();
+  }, [id]);
+
   return (
     <div id={id}>
-      <img />
+      {loading && <b>Loading character icon...</b>}
+      {error && <b>Error loading character icon.</b>}
+      <img src={characterIcon} alt="character icon" width={100} />
       <h3>{name}</h3>
       <ul>
         <li>Rarity: {rarity}</li>
